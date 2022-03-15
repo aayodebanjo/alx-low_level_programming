ac5cab614652ff8775ecb1ef9a3db48855b9c550
aayodebanjo <adebanjoayomide15@gmail.com>
2022-03-15 10:24:27 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,45 @@
 #include "main.h"
 
+
+
 /**
- * main - prints _putchar
  *
- * Description: prints _putchar
+ *  * main - prints _putchar
  *
- * Return: 0 (Success)
- */
+ *   *
+ *
+ *    * Description: prints _putchar
+ *
+ *     *
+ *
+ *      * Return: 0 (Success)
+ *
+ *       */
+
+
 
 int main(void)
+
 {
-	char text[9] = "_putchar";
-	int i = 0;
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
+		char text[9] = "_putchar";
 
-	return (0);
+			int i = 0;
+
+
+
+				for (i = 0; i < 8; i++)
+
+						{
+
+									_putchar(text[i]);
+
+										}
+
+					_putchar('\n');
+
+
+
+						return (0);
+
 }
