49673b8027bc24f65d8ca4510fd457aac6873796
Ayomide Adebanjo <95983121+aayodebanjo@users.noreply.github.com>
2022-03-15 14:40:13 +0100

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,19 @@
 #include <unistd.h>
-
-#include "holberton.h"
-
-
+#include "_putchar.h"
 
 /**
- *
- *  * main - Check description
- *
- *   * Description: It prints the word Holberton, followed by a new line.
- *
- *    * Return: 0.
- *
- *     */
-
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
 int main(void)
-
 {
+	char word[8] = "_putchar";
+	int i;
 
-		char word[9] = "Holberton";
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
 
-			int i;
-
-
-
-				for (i = 0; i < 9; i++)
-
-							_putchar(word[i]);
-
-					_putchar('\n');
-
-
-
-						return (0);
-
+	return (0);
 }
