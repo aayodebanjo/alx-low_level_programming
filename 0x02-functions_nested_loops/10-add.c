3ff5ddbf1fcdaaed8b9fe0d7cdbf54678b341f4e
Ayomide Adebanjo <95983121+aayodebanjo@users.noreply.github.com>
2022-03-15 23:10:26 +0100

    commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,39 +1,20 @@
 #include "main.h"
 
-
-
 /**
+ * add - adds two integers
+ * @n1: integer 1
+ * @n2: integer 2
  *
- *  * add - adds two integers
+ * Description: give two integers, returns sum
  *
- *   * @n1: integer 1
- *
- *    * @n2: integer 2
- *
- *     *
- *
- *      * Description: give two integers, returns sum
- *
- *       *
- *
- *        * Return: sum
- *
- *         */
-
-
+ * Return: sum
+ */
 
 int add(int n1, int n2)
-
 {
+	int sum;
 
-		int sum;
+	sum = n1 + n2;
 
-
-
-			sum = n1 + n2;
-
-
-
-				return (sum);
-
+	return (sum);
 }
