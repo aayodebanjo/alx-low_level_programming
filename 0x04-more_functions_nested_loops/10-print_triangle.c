d39e33ba076aaacc8b40ef511843c2ca522e1009
Ayomide Adebanjo <95983121+aayodebanjo@users.noreply.github.com>
2022-03-17 14:02:34 +0100

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,71 +1,36 @@
 #include "main.h"
 
-
-
 /**
+ * print_triangle - prints a triangle
+ * @size: length of sides minus hypotenuse
  *
- *  * print_triangle - prints a triangle
- *
- *   * @size: length of sides minus hypotenuse
- *
- *    *
- *
- *     * Return: void
- *
- *      */
-
-
+ * Return: void
+ */
 
 void print_triangle(int size)
-
 {
+	int line = 0;
+	int spaces;
+	int hashes;
 
-		int line = 0;
-
-			int spaces;
-
-				int hashes;
-
-
-
-					if (size > 0)
-
-							{
-
-										while (line < size)
-
-													{
-
-																	for (spaces = size - 1; spaces > line; spaces--)
-
-																					{
-
-																										_putchar(' ');
-
-																													}
-
-																				for (hashes = 0; hashes < line + 1; hashes++)
-
-																								{
-
-																													_putchar('#');
-
-																																}
-
-																							_putchar('\n');
-
-																										line++;
-
-																												}
-
-											}
-
-						else
-
-								{
-
-											_putchar('\n');
-
-												}
-
+	if (size > 0)
+	{
+		while (line < size)
+		{
+			for (spaces = size - 1; spaces > line; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 0; hashes < line + 1; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			line++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
