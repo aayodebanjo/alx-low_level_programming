d48ad9be2e79414316f184ba17335dfd57f07506
Ayomide Adebanjo <95983121+aayodebanjo@users.noreply.github.com>
2022-03-28 14:51:27 +0100

    5-main.c

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -1,35 +1,18 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
+ * main - check the code for Holberton School students.
  *
- *  * main - check the code for Holberton School students.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	char *s = "wworld of this";
+	char *f = "world";
+	char *t;
 
-		char *s = "wworld of this";
-
-			char *f = "world";
-
-				char *t;
-
-
-
-					t = _strstr(s, f);
-
-						printf("%s\n", t);
-
-							return (0);
-
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	return (0);
 }
