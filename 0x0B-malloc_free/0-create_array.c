f4c4774a37826ffce0d643092bacca1d4c2cf4a0
aayodebanjo <adebanjoayomide15@gmail.com>
2022-04-04 05:57:10 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,53 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#include "main.h"
 
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
+#include <stdlib.h>
 
-#endif
+/**
+ *
+ *  * create_array - creates an array of chars.
+ *
+ *   * @size: size of the array.
+ *
+ *    * @c: storaged char
+ *
+ *     *
+ *
+ *      * Return: pointer of an array of chars
+ *
+ *       */
+
+char *create_array(unsigned int size, char c)
+
+{
+
+		char *cr;
+
+			unsigned int i;
+
+
+
+				if (size == 0)
+
+							return (NULL);
+
+
+
+					cr = malloc(sizeof(c) * size);
+
+
+
+						if (cr == NULL)
+
+									return (NULL);
+
+
+
+							for (i = 0; i < size; i++)
+
+										cr[i] = c;
+
+
+
+								return (cr);
+
+}
