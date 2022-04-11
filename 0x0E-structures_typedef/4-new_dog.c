8127096fab4770bdc1f9fbc3340d8de3d02cf846
Ayomide Adebanjo <95983121+aayodebanjo@users.noreply.github.com>
2022-04-11 20:36:15 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 
-
 /**
  * new_dog - creates a new dog.
  * @name: name of the dog.
@@ -15,6 +14,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p_dog;
 	int i, lname, lowner;
+
 	p_dog = malloc(sizeof(*p_dog));
 	if (p_dog == NULL || !(name) || !(owner))
 	{
@@ -22,10 +22,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (lname = 0; name[lname]; lname++);
-	for (lowner = 0; owner[lowner]; lowner++);
+	for (lname = 0; name[lname]; lname++)
+		;
+
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
 	p_dog->name = malloc(lname + 1);
 	p_dog->owner = malloc(lowner + 1);
+
 	if (!(p_dog->name) || !(p_dog->owner))
 	{
 		free(p_dog->owner);
@@ -33,13 +38,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p_dog);
 		return (NULL);
 	}
+
 	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
 	p_dog->name[i] = '\0';
+
 	p_dog->age = age;
+
 	for (i = 0; i < lowner; i++)
 		p_dog->owner[i] = owner[i];
 	p_dog->owner[i] = '\0';
-	return (p_dog);
 
+	return (p_dog);
 }
