74b53a82f83de578191c0120d1258d7fa1c556d2
aayodebanjo <adebanjoayomide15@gmail.com>
2022-04-13 04:31:30 -0700

    print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+
+
+/**
+ *
+ *  * print_name - prints a name.
+ *
+ *   * @name: input name.
+ *
+ *    * @f: function pointer.
+ *
+ *     *
+ *
+ *      * Return: no return.
+ *
+ *       */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+
+		if (name && f)
+
+					f(name);
+
+}
+
+© 2022 GitHub, Inc.
